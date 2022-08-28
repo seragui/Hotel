25249b9aeb748e54070a16a31b67134d388f8dda
Belserion21 <rasa0821@hotmail.com>
2022-08-28 01:12:39 -0600

    Version 4

--- Reserva/models.py
+++ Reserva/models.py
@@ -1,4 +1,3 @@
-from tkinter import CASCADE
 from django.db import models
 from base.models import BaseModel
 from .models import *
@@ -29,3 +28,42 @@ class Reserva(BaseModel):
     class Meta:
         verbose_name = ('Reserva')
         verbose_name_plural = ('Reservas')
+
+
+class ServiciosReserva(BaseModel):
+    nombre_servicio = models.CharField(
+        'Nombre de Servicio',  max_length=100, blank=False, null=False)
+    descripcion = models.TextField('Descripción', blank=False, null=False)
+    precio = models.FloatField('Precio', blank=False, null=False)
+    capacidad = models.CharField(
+        'Capacidad', max_length=50, blank=False, null=False, default='')
+
+    class Meta:
+        verbose_name = ('Servicio')
+        verbose_name_plural = ('Servicios')
+
+    def __str__(self):
+        return self.nombre_servicio
+
+
+class HorariosServiciosReserva(BaseModel):
+    id_servicio = models.ForeignKey("Servicios", on_delete=models.CASCADE)
+    d = (
+        ('Lunes', 'Lunes'),
+        ('Martes', 'Martes'),
+        ('Miercoles', 'Miercoles'),
+        ('Jueves', 'Jueves'),
+        ('Viernes', 'Viernes'),
+        ('Sábado', 'Sábado'),
+        ('Domingo', 'Domingo'),
+    )
+    dias = models.CharField('Dia', max_length=50,
+                            blank=False, null=False, choices=d)
+    hora = models.CharField('Hora', max_length=50, blank=False, null=False)
+
+    class Meta:
+        verbose_name = ('Horario')
+        verbose_name_plural = ('Horarios')
+
+    def __str__(self):
+        return str(self.id) 
